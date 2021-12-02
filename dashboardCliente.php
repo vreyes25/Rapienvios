b9d61b8e89c763b1241533a53e76a723904a053b
Roger Sifontes <roferhn@gmail.com>
2021-12-01 19:09:44 -0600

    clienter

---
+++ dashboardCliente.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="img/RLogo.png" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/dashboard.css" />
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="js/jquery.min.js"></script>
+    <title>Rapienvios | Dashboard</title>
+  </head>
+  <body>
+    <div class="side-panel">
+      <img src="img/Logo.png" alt="logo" id="logo" />
+      <div class="decoration-line"></div>
+      <ul class="dashboard-elements">
+        <li class="active">
+          <i class="ri-folder-user-fill side-icons"></i>
+          <a href="#">Clientes</a>
+        </li>
+        <li>
+          <i class="ri-dropbox-fill side-icons"></i>
+          <a href="#">Paquetes</a>
+        </li>
+        <li>
+          <i class="ri-price-tag-3-fill side-icons"></i>
+          <a href="#">Precios</a>
+        </li>
+        <li>
+          <i class="ri-user-fill side-icons"></i>
+          <a href="#">Empleados</a>
+        </li>
+        <li class="logout">
+          <i class="ri-logout-box-r-line side-icons"></i>
+          <a href="index.php">Cerrar Sesión</a>
+        </li>
+      </ul>
+    </div>
+    <div class="content-data">
+      <div class="up-content">
+        <div class="searchBar">
+          <input type="text" id="search" placeholder="Buscar" />
+          <i class="ri-search-line searchButton"></i>
+        </div>
+        <div class="user-name">
+          <h3>Bienvenido, <strong>Victor Reyes</strong></h3>
+        </div>
+      </div>
+      <div class="data">
+        <div class="up-content">
+          <h3>Total de clientes: <strong>50</strong></h3>
+          <button type="button" id="nuevoCliente" class="startSession">
+            Nuevo
+          </button>
+        </div>
+        <div class="content-tables"></div>
+      </div>
+    </div>
+    <div id="miModal" class="modal">
+      <div class="flex" id="flex">
+        <div class="contenido-modal">
+          <div class="modal-header flex">
+            <i class="ri-folder-user-fill side-icons"></i>
+            <h2>Nuevo Cliente</h2>
+            <span class="close" id="close">&times;</span>
+          </div>
+          <div class="modal-body">
+            <form action="" method="post" class="nuevoCliente">
+              <div class="form">
+                <input type="text" placeholder="ID Cliente" disabled/>
+                <input type="text" placeholder="Ingrese el nombre"  id="nombreCliente"/>
+                <input type="text" placeholder="Ingrese el teléfono" id="telefono" />
+                <input type="text" placeholder="Ingrese la dirección" id="direccion"/>
+                <button type="button" onclick="registrarCliente()" class="guardarCliente">Guardar</button>
+              </div>
+              <div class="imagen">
+                <img src="img/nuevoCliente.svg" alt="ilustracion" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="js/main.js"></script>
+  </body>
+</html>
